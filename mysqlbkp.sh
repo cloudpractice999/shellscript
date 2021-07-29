3c38daf204940d76f918f38d2481fdbe08126d25
root <root@ip-172-31-5-36.ap-south-1.compute.internal>
2021-07-29 13:43:44 +0000

    commit

---
+++ mysqlbkp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+v_dbname=$1
+v_username=$(cat cred |grep username |awk -F"=" '{print $2}')
+v_password=$(cat cred |grep password |awk -F"=" '{print $2}')
+v_dbhost=db_host.rds.asw.com
+
+echo "mysqldump -h $v_dbhost -u $v_username -p $v_password $v_dbname > /db-backup/$v_dbname.sql"
