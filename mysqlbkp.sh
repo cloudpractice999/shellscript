39f314bfec1cd3169618d89aead6237696c268de
cloudpractice999 <87352417+cloudpractice999@users.noreply.github.com>
2021-08-03 08:02:56 +0530

    Delete mysqlbkp.sh

--- mysqlbkp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this is a test-removed
-v_dbname=$1
-v_username=$(cat cred |grep username |awk -F"=" '{print $2}')
-v_password=$(cat cred |grep password |awk -F"=" '{print $2}')
-v_dbhost=db_host.rds.asw.com
-
-echo "mysqldump -h $v_dbhost -u $v_username -p $v_password $v_dbname > /db-backup/$v_dbname.sql"
